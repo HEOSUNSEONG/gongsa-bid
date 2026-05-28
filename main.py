d2f7f68dfd5800d1fe27beab2bcb28297310f52e
HEOSUNSEONG <dream9473@nate.com>
2026-05-28 14:51:23 +0900

    main.py에서 index.html 화면 연결

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <title>gongsa-bid</title>
-    </head>
-    <body>
-        <h1>gongsa-bid 테스트 웹사이트입니다.</h1>
-        <p>건설회사 전용 나라장터 맞춤공고 추천 웹사이트를 만들고 있습니다.</p>
-        <p>1차 목표: 송원건설 기준 맞춤 공고 추천 화면 만들기</p>
-    </body>
-    </html>
-    """
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request
+        }
+    )
 
 
 @app.get("/health")
